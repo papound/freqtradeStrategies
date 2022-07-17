1b49af345d29e50250a3daec4744a07ea8c4710d
papound <chanawatnpound@gmail.com>
2022-07-17 15:45:33 +0700

    import pine script methods

--- jul152022Strategy.py
+++ jul152022Strategy.py
@@ -134,6 +134,39 @@ class Jul152022Strategy(IStrategy):
     1.            Green Line  Cross Down   White Line  (LFS)
     '''
 
+    def na(self, val):
+        return val != val
+
+    def nz(self, x, y=None):
+        if isinstance(x, np.generic):
+            return x.fillna(y or 0)
+        if x != x:
+            if y is not None:
+                return y
+            return 0
+        return x
+
+    def barssince(self, condition, occurrence=0):
+        cond_len = len(condition)
+        occ = 0
+        since = 0
+        res = float('nan')
+        while cond_len - (since+1) >= 0:
+            cond = condition[cond_len-(since+1)]
+            if cond and not cond != cond:
+                if occ == occurrence:
+                    res = since
+                    break
+                occ += 1
+            since += 1
+        return res
+
+    def valuewhen(self, condition, source, occurrence=0):
+        res = float('nan')
+        since = self.barssince(condition, occurrence)
+        if since is not None:
+            res = source[-(since+1)]
+        return res
     def informative_pairs(self):
         """
         Define additional, informative pair/interval combinations to be cached from the exchange.
